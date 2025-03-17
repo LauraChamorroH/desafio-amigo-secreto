dd0e091762153034d68aec0a8b1afecf29699cfc
Laura Chamorro <chamorro.lv@gmail.com>
2025-03-17 16:00:41 -0500

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+//Función para asignarle valores a un elemento del html
+function asignarValorElemento (elemento,valor){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = valor;
+}
+
+
+//Función para que la persona agregue un nombre al hacer click en el botón
+function agregarAmigo(){
+    alert('Click desde el botón');
+}
+
+//Ejecuta la función de asignar un valor al hacer click
+asignarValorElemento ('onclick','Primero');
